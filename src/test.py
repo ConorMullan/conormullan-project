32786f6ff9e279e62300ecf4cf88de64b026abf7
ConorMullan <43961027+ConorMullan@users.noreply.github.com>
2018-11-20 07:22:40 +0000

    Latest version

--- src/test.py
+++ src/test.py
@@ -1,15 +1,15 @@
 
-import tweepy as tp
+# import tweepy as tp
 
 
-tweet = "First Test Tweet"
-try:
-
-    if api.update_status(tweet):
-        print("Posted")
-
-except tp.TweepError as e:
-    print(e)
+#
+# try:
+#
+#     if api.update_status(tweet):
+#         print("Posted")
+#
+# except tp.TweepError as e:
+#     print(e)
 
 # public_tweets = api.home_timeline()
 #
--- test/test_authorization.py
+++ test/test_authorization.py
@@ -1,12 +1,13 @@
-import unittest
-from src.auth import Auth
-
-
-class TestAuthorization(unittest.TestCase):
-
-    def test_authorization(self):
-        auth = Auth()
-
-
-if __name__ == '__main__':
-    unittest.main()
+# import unittest
+# from src.auth import Auth
+#
+#
+# class TestAuthorization(unittest.TestCase):
+#
+#     def test_authorization(self):
+#         self.auth = Auth()
+#         return self.auth.authorize()
+#
+#
+# if __name__ == '__main__':
+#     unittest.main()
